fae2b6e509a3f3adbda6733e6082e4d38cd1bb0c
seiyashun <seiyashun96@yahoo.com>
2017-11-30 08:28:18 +0700

    update

--- app/src/main/java/com/tangtuongco/apppppppppppp/activity/DangKyActivity.java
+++ app/src/main/java/com/tangtuongco/apppppppppppp/activity/DangKyActivity.java
@@ -2,6 +2,8 @@ package com.tangtuongco.apppppppppppp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.tangtuongco.apppppppppppp.R;
 
@@ -11,5 +13,16 @@ public class DangKyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dang_ky);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("Đăng Ký");
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
